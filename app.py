78b794e588108aabfc1a0efda27170e586cf9b01
Ihor N <js.rcnit@gmail.com>
2020-05-05 21:08:21 +0300

    flask forms

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, ValidationError
+from wtforms.validators import Email, DataRequired, EqualTo
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfdsgbkoeufdhiuoroyxmunfyriwjzmeyzo'
+
+@app.route('/')
+def index() :
+    return render_template('index.html')
+
+class RegisterForm(FlaskForm) :
+    username = StringField('USERNAME', validators=[ DataRequired() ])
+    email = StringField('EMAIL', validators=[ Email() ])
+    password1 = StringField('PASSWORD', validators=[ DataRequired() ])
+    password2 = StringField('PASSWORD AGAIN', validators=[ DataRequired(), EqualTo('password1') ])
+
+    def validate_password1(self, field) :
+        if len(field.data) < 8 or len(field.data) > 13 :
+            raise ValidationError('My validator Password must be from 8 to 13 characters')
+
+   
+
+
+@app.route('/register', methods=[ 'GET', 'POST' ])
+def register() :
+    reg = RegisterForm()
+
+    if reg.validate_on_submit():
+        return redirect('/success')
+
+    return render_template('register.html', regform = reg)
+
+if __name__ == '__main__' :
+    app.run(debug=True)
