170d2ee7dd0618c059fc74e6d0312446fbc50856
Ihor N <js.rcnit@gmail.com>
2020-05-05 21:11:18 +0300

    flask forms2

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class RegisterForm(FlaskForm) :
     password1 = StringField('PASSWORD', validators=[ DataRequired() ])
     password2 = StringField('PASSWORD AGAIN', validators=[ DataRequired(), EqualTo('password1') ])
 
-    def validate_password1(self, field) :
+    def validate_password2(self, field) :
         if len(field.data) < 8 or len(field.data) > 13 :
             raise ValidationError('My validator Password must be from 8 to 13 characters')
 
@@ -29,6 +29,13 @@ def register() :
     reg = RegisterForm()
 
     if reg.validate_on_submit():
+        # reg.username.data
+        # reg.email.data
+        # db.session.add(User)
+
+
+
+
         return redirect('/success')
 
     return render_template('register.html', regform = reg)
